6a3d886436cf1a732a124ff2f6373f45f0261867
Nam <namdeveloper.ca@gmail.com>
2024-05-17 15:50:24 +0700

    drizzle setup

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+  dialect: 'postgresql', // "mysql" | "sqlite" | "postgresql"
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+})
